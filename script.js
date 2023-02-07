40e4b13ef8372520053fcb55d7bb807cdd390dc3
Calum Childs <calumchilds@hotmail.co.uk>
2023-02-07 19:37:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function toggleNavigation() {
+    var nav = document.getElementById("site-navigation");
+    if (nav.className === "main-navigation") {
+        nav.className += " toggled";
+    } else {
+        nav.className = "main-navigation";
+    }
+}
